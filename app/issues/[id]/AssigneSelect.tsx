53f260da6333b1bf83e393a5331834c2e95a53b2
Josias Guzman <jguzman@aoiths.org>
2023-11-21 11:19:50 -0500

    Populate the assginee

--- app/issues/[id]/AssigneSelect.tsx
+++ app/issues/[id]/AssigneSelect.tsx
@@ -1,21 +1,38 @@
 'use client';
 
-import { Select } from '@radix-ui/themes'
-import React from 'react'
+import { User } from '@prisma/client';
+import { Select } from '@radix-ui/themes';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const AssigneSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  // useEffect calls the backend
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {data} = await axios.get<User[]>('/api/users');
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
-      <Select.Trigger placeholder='Assign...'>
+      <Select.Trigger placeholder="Assign..."/>
         <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                <Select.Item value="1">Josias Guzman</Select.Item>
-            </Select.Group>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            {users.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
         </Select.Content>
-      </Select.Trigger>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneSelect
+export default AssigneSelect;
