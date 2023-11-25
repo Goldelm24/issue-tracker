01c14dfa49a24678a66e7de991b18d1f70efbe5e
Josias Guzman <jguzman@aoiths.org>
2023-11-25 16:54:34 -0500

    Change relationMode to prisma

--- prisma/migrations/20231122165930_add_assigned_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Issue` ADD COLUMN `assginedToUserId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assginedToUserId_fkey` FOREIGN KEY (`assginedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
